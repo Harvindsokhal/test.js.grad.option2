c8ea58a01c1cde681960f67c5214f50db9455f5e
Harvindsokhal <harvindsokhal@gmail.com>
2021-07-11 18:02:42 +0100

    Task 3

--- src/03-api-call/index.js
+++ src/03-api-call/index.js
@@ -4,7 +4,7 @@ module.exports = async function fetchLukeSkywalker() {
     const url = 'https://swapi.dev/api/people/1/';
     const response = await fetch(url);
     const data = await response.json();
-    const replaced = JSON.stringify(data).replace(/https/g, "http")
+    const replaced = JSON.stringify(data).replace(/https/g, "http") //API has web links stored as https and was changed to http to match the snapshot.
     const converted = JSON.parse(replaced)
     return (converted);
 };
