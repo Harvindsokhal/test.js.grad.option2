e5dc58662053cb6d8dda178594c8ee6c4f506b5c
Harvindsokhal <harvindsokhal@gmail.com>
2021-07-11 17:41:04 +0100

    Task 2

--- src/02-create-base/index.js
+++ src/02-create-base/index.js
@@ -1,8 +1,13 @@
 // You can assume the input is an integer
 
-// Average and worst case time complexity (Big-O) of my function is: 
-// Explain: 
+// Average and worst case time complexity (Big-O) of my function is: O(1)
+/**
+ Explain: Like the last algorithm this one also has a constant time complexity as it is only carrying out one arithmeric equation 
+ which means the time complexity would be O(1 + 1) which is simplified to O(1).
+*/
 
 module.exports = function createBase(integer) {
-  //TODO
+  return function (integer2) { 
+    return integer + integer2
+  };
 };
