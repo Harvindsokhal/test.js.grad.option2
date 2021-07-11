7fbded48314c114e3265e9048f429e349a61c434
Harvindsokhal <harvindsokhal@gmail.com>
2021-07-11 17:42:45 +0100

    Task 3

--- src/03-api-call/index.js
+++ src/03-api-call/index.js
@@ -1,5 +1,11 @@
+const fetch = require('node-fetch');
 
 module.exports = async function fetchLukeSkywalker() {
-    //TODO
+    const url = 'https://swapi.dev/api/people/1/';
+    const response = await fetch(url);
+    const data = await response.json();
+    const replaced = JSON.stringify(data).replace(/https/g, "http")
+    const converted = JSON.parse(replaced)
+    return (converted);
 };
 
