e6efd0dec3d0462b1978a14ae3900df58cc1d0c7
Harvindsokhal <harvindsokhal@gmail.com>
2021-07-11 17:58:39 +0100

    Task 4

--- src/04-sort-array/index.js
+++ src/04-sort-array/index.js
@@ -7,10 +7,23 @@
  *   - Not required, but major bonus points awarded
  */
 
-// Average and worst case time complexity (Big-O) of my function is:
-// Explain:
+// Average and worst case time complexity (Big-O) of my function is: O(n log(n))
+/**
+  Explain: Due to the array containing more than 10 elements it is fairly large and with javascripts sorting algorithm it would have an average time complexity of O(n log(n)) to sort,
+  although if an array had 10 or fewer elements it would have an average time complexity of O(n^2).
+ */
 
+var check = require('check-types');
 
 module.exports = function sortArray(arr) {
-    //TODO
+    if (check.emptyArray(arr)) { 
+        return [] 
+    } else if (!check.array.of.integer(arr)) {
+        throw TypeError('Input is not integer') ;
+    } else {
+        arr.sort(function(a, b) {
+            return a - b;
+        })
+        return arr
+    }
 };
