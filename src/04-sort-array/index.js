e63b8ad9922bac617a955b5fca370c857bf83d53
Harvindsokhal <harvindsokhal@gmail.com>
2021-07-11 18:10:49 +0100

    Task 4

--- src/04-sort-array/index.js
+++ src/04-sort-array/index.js
@@ -1,12 +1,3 @@
-/**
- * LEVEL 1: Implement using Array.prototype.sort method and NPM packages (e.g.: check-types)
- *   - REQUIRED
- * LEVEL 2: Implement without using Array.prototype.sort method or libraries, without regard to efficiency
- *   - Not required, but bonus points awarded
- * LEVEL 3: Implement without using Array.prototype.sort method or libraries, efficiently
- *   - Not required, but major bonus points awarded
- */
-
 // Average and worst case time complexity (Big-O) of my function is: O(n log(n))
 /**
   Explain: Due to the array containing more than 10 elements it is fairly large and with javascripts sorting algorithm it would have an average time complexity of O(n log(n)) to sort,
