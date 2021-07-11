ac5dbb0cd3b1918b54cf0f45e8177776260e57c9
Harvindsokhal <harvindsokhal@gmail.com>
2021-07-11 17:36:58 +0100

    Task 1

--- src/01-reverse-words/index.js
+++ src/01-reverse-words/index.js
@@ -4,11 +4,11 @@
  *   - contains words that are divided by a single space
  */
 
-// Average and worst case time complexity (Big-O) of my function is: 
-// Explain: 
+// Average and worst case time complexity (Big-O) of my function is: O(1)
+// Explain: The Algorithm has a constant time complexity as it is not dependent on the size of the input data, and the time required to run is the same every single time.
 
 
 
 module.exports = function reverseWordsInSentence(input) {
-  //TODO
+  return input.split("").reverse().join("").split(" ").reverse().join(" ")
 };
